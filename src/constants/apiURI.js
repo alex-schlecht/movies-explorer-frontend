6d431c78cb34e838c3e8e1baeb6c99969ac316dc
alex-schlecht <113642938+alex-schlecht@users.noreply.github.com>
2023-08-03 06:25:02 +0300

    pre-merge

--- src/constants/apiURI.js
+++ src/constants/apiURI.js
@@ -1,4 +1,4 @@
-//export const API_URI = "//api.movie.nomoredomains.work";
-const { NODE_ENV } = process.env;
+//const { NODE_ENV } = process.env;
+//export const API_URI = NODE_ENV === 'production' ? '//api.movie.nomoredomains.work' : 'http://localhost:3000';
 export const MOVIES_API_URI = "https://api.nomoreparties.co";
-export const API_URI = NODE_ENV === 'production' ? '//api.movie.nomoredomains.work' : 'http://localhost:3000';
+export const API_URI = "//api.movie.nomoredomains.work";
